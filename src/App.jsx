3c8fcd46720b5994a7ef252547703a3fd50fb8e1
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-08 11:50:20 +0200

    added a new file 'server.js' which houses the api data that was called via the miragejs dependency that I installed. also added a new state function in the file 'Vans.jsx' that fetches the api and utilizes useState and useEffect to set the data and use it in the 'vanElements' variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,28 @@
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
-import { Vans } from "../presentation/Vans";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./index.css";
+import { Home } from "../presentation/Home.jsx";
+import { About } from "../presentation/About.jsx";
+import { Vans } from "../presentation/Vans.jsx";
 
-export const App = () => {
+import "./server.js";
+
+export default function App () {
 
   return (
     <BrowserRouter>
     <header>
-      <Link className="site-logo" to="/presentation/Home.jsx">#VanLife</Link>
+      <Link className="site-logo" to="/">#VanLife</Link>
       <nav>
-        <Link to="/presentation/About.jsx">About</Link>
-        <Link to="/presentation/Vans.jsx">Vans</Link>
+        <Link to="/about">About</Link>
+        <Link to="/vans">Vans</Link>
       </nav>
     </header>
     <Routes>
-      <Route path="/presentation/Home.jsx" element={<Home/>}/>
-      <Route path="/presentation/About.jsx" element={<About/>}/>
-      <Route path="/presentation/Vans.jsx" element={<Vans/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/vans" element={<Vans/>}/>
     </Routes>
     </BrowserRouter>
   )
