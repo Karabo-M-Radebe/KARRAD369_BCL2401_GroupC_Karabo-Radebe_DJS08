fa5b0ae1e7263a1897be3ef95e6fccb8e517363f
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-09 01:45:39 +0200

    created new component files for the Header and the Layout then extracted the Header code from App.jsx and nested the routes in a route tag with a Layout element from Layout.jsx

---
+++ components/Layout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "./Header.jsx";
+
+export const Layout = () => {
+    return (
+        <>
+        <Header />
+        <Outlet/>
+        </>
+        
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,19 @@ import { Vans } from "../presentation/Vans.jsx";
 
 import "./server.js";
 import { VanDetail } from "../presentation/VanDetail.jsx";
+import { Layout } from "../components/Layout.jsx";
 
 export default function App () {
 
   return (
     <BrowserRouter>
-    <header>
-      <Link className="site-logo" to="/">#VanLife</Link>
-      <nav>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
-      </nav>
-    </header>
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/vans" element={<Vans/>}/>
-      <Route path="/vans/:id" element={<VanDetail/>}/>
+      <Route element={<Layout/>}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/vans" element={<Vans/>}/>
+        <Route path="/vans/:id" element={<VanDetail/>}/>
+      </Route>
     </Routes>
     </BrowserRouter>
   )
