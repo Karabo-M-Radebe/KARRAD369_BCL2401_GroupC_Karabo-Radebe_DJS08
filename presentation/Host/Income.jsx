2a8b727c46c3eae6033f3fe0499c713639c94917
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-10 05:15:46 +0200

    "Added new route for Host, removed VanDetail and Vans components, and updated Header with new link"

---
+++ presentation/Host/Income.jsx
@@ -0,0 +1,7 @@
+
+
+export const Income = () => {
+    return(
+    <h1>Income</h1>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./index.css";
 import { Home } from "../presentation/Home.jsx";
 import { About } from "../presentation/About.jsx";
-import { Vans } from "../presentation/Vans.jsx";
+import { Vans } from "../presentation/Vans/Vans.jsx";
 
 import "./server.js";
-import { VanDetail } from "../presentation/VanDetail.jsx";
+import { VanDetail } from "../presentation/Vans/VanDetail.jsx";
 import { Layout } from "../components/Layout.jsx";
+import { Dashboard } from "../presentation/Host/Dashboard.jsx";
+import { Income } from "../presentation/Host/Income.jsx";
+import { Reviews } from "../presentation/Host/Reviews.jsx";
 
 export default function App () {
 
@@ -19,6 +22,9 @@ export default function App () {
         <Route path="/about" element={<About/>}/>
         <Route path="/vans" element={<Vans/>}/>
         <Route path="/vans/:id" element={<VanDetail/>}/>
+        <Route path="/host" element={<Dashboard/>}/>
+        <Route path="/host/income" element={<Income/>}/>
+        <Route path="/host/reviews" element={<Reviews/>}/>
       </Route>
     </Routes>
     </BrowserRouter>
